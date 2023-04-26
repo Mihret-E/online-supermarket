bca3b59a72eae693b8ca3d4d43c87ca06d157c99
Mihret Engida <delilajolie@gmail.com>
2023-04-26 10:53:12 +0300

    created springboot project

---
+++ src/main/java/com/act/onlinesupermarket/OnlinesupermarketApplication.java
@@ -0,0 +1,14 @@
+package com.act.onlinesupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinesupermarketApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OnlinesupermarketApplication.class, args);
+    }
+
+}
